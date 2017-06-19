ab38993caf1e4969b0cc51fb46f9b8dd4e66efad
srinivas <srinivasu.lanka@balfour.com>
2017-06-19 16:05:38 -0500

    Fork ansible elastic version in balfour branch

--- test/integration/helpers/serverspec/package_spec.rb
+++ test/integration/helpers/serverspec/package_spec.rb
@@ -74,12 +74,11 @@ shared_examples 'package::init' do  |es_version,plugins|
       it { should be_directory }
       it { should be_owned_by 'elasticsearch' }
     end
-    #confirm plugins are installed and the correct version
-    describe command('curl -s localhost:9200/_nodes/plugins | grep \'"name":"'+plugin+'","version":"'+es_version+'"\'') do
+
+    describe command('curl -s localhost:9200/_nodes/plugins?pretty=true | grep '+plugin) do
       its(:exit_status) { should eq 0 }
     end
   end
-
 
   describe file('/etc/init.d/elasticsearch') do
     it { should_not exist }
--- test/integration/helpers/serverspec/standard_spec.rb
+++ test/integration/helpers/serverspec/standard_spec.rb
@@ -19,12 +19,7 @@ shared_examples 'standard::init' do |es_version|
     it { should be_owned_by 'elasticsearch' }
   end
 
-  describe file('/etc/elasticsearch/node1/log4j2.properties') do
-    it { should be_file }
-    it { should be_owned_by 'elasticsearch' }
-  end
-
-  describe file('/etc/elasticsearch/node1/jvm.options') do
+  describe file('/etc/elasticsearch/node1/logging.yml') do
     it { should be_file }
     it { should be_owned_by 'elasticsearch' }
   end
@@ -34,6 +29,7 @@ shared_examples 'standard::init' do |es_version|
     it { should contain 'cluster.name: elasticsearch' }
     it { should contain 'path.conf: /etc/elasticsearch/node1' }
     it { should contain 'path.data: /var/lib/elasticsearch/localhost-node1' }
+    it { should contain 'path.work: /tmp/elasticsearch/localhost-node1' }
     it { should contain 'path.logs: /var/log/elasticsearch/localhost-node1' }
   end
 
